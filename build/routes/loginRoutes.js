cdecefd6a524689019006c8c75df73fce8409d69
Todd Garrison <toddlgarrison@gmail.com>
2023-08-29 14:46:27 -0400

    Update Type definition for 'body'

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -21,5 +21,10 @@ router.get('/login', (req, res) => {
 });
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
-    res.send(email.toUpperCase());
+    if (email) {
+        res.send(email.toUpperCase());
+    }
+    else {
+        res.send('You must provide an email property.');
+    }
 });
