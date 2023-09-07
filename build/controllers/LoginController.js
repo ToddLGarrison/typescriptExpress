99b533cc619d135965907ce31d607a903036781b
Todd Garrison <toddlgarrison@gmail.com>
2023-09-07 16:36:13 -0400

    Additional set up for decorators

--- build/controllers/LoginController.js
+++ build/controllers/LoginController.js
@@ -1,78 +1,30 @@
 "use strict";
-var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
-    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
-    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
-    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
-    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
-    var _, done = false;
-    for (var i = decorators.length - 1; i >= 0; i--) {
-        var context = {};
-        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
-        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
-        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
-        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
-        if (kind === "accessor") {
-            if (result === void 0) continue;
-            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
-            if (_ = accept(result.get)) descriptor.get = _;
-            if (_ = accept(result.set)) descriptor.set = _;
-            if (_ = accept(result.init)) initializers.unshift(_);
-        }
-        else if (_ = accept(result)) {
-            if (kind === "field") initializers.unshift(_);
-            else descriptor[key] = _;
-        }
-    }
-    if (target) Object.defineProperty(target, contextIn.name, descriptor);
-    done = true;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
-    var useValue = arguments.length > 2;
-    for (var i = 0; i < initializers.length; i++) {
-        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
-    }
-    return useValue ? value : void 0;
-};
-var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
-    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
-    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-let LoginController = (() => {
-    let _classDecorators = [controller('/')];
-    let _classDescriptor;
-    let _classExtraInitializers = [];
-    let _classThis;
-    let _instanceExtraInitializers = [];
-    let _getLogin_decorators;
-    var LoginController = _classThis = class {
-        getLogin(req, res) {
-            res.send(`
-            <form method="POST">
-            <div>
-                <label>Email</label>
-                <input name="email" />
-            </div>
-            <div>
-                <label>Password</label>
-                <input name="password" type="password" />
-            </div>
-            <button>Submit</button>
-            </form>
-        `);
-        }
-        ;
-        constructor() {
-            __runInitializers(this, _instanceExtraInitializers);
-        }
+var routes_1 = require("./decorators/routes");
+var LoginController = /** @class */ (function () {
+    function LoginController() {
+    }
+    LoginController.prototype.getLogin = function (req, res) {
+        res.send("\n            <form method=\"POST\">\n            <div>\n                <label>Email</label>\n                <input name=\"email\" />\n            </div>\n            <div>\n                <label>Password</label>\n                <input name=\"password\" type=\"password\" />\n            </div>\n            <button>Submit</button>\n            </form>\n        ");
     };
-    __setFunctionName(_classThis, "LoginController");
-    (() => {
-        _getLogin_decorators = [get('/login')];
-        __esDecorate(_classThis, null, _getLogin_decorators, { kind: "method", name: "getLogin", static: false, private: false, access: { has: obj => "getLogin" in obj, get: obj => obj.getLogin } }, null, _instanceExtraInitializers);
-        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name }, null, _classExtraInitializers);
-        LoginController = _classThis = _classDescriptor.value;
-        __runInitializers(_classThis, _classExtraInitializers);
-    })();
-    return LoginController = _classThis;
-})();
+    ;
+    __decorate([
+        (0, routes_1.get)('/login'),
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Object, Object]),
+        __metadata("design:returntype", void 0)
+    ], LoginController.prototype, "getLogin", null);
+    LoginController = __decorate([
+        controller('/')
+    ], LoginController);
+    return LoginController;
+}());
--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express'
-
+import { get } from './decorators/routes'
 @controller('/')
 class LoginController {
     @get('/login')
