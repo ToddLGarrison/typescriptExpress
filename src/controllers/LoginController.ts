0eca9b275d3addd930ea9540fdbaad377bf41872
Todd Garrison <toddlgarrison@gmail.com>
2023-09-08 15:21:10 -0400

    refactor

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
-import { Router, Request, Response, NextFunction } from 'express'
-import { get, controller } from './decorators'
+import {  Request, Response, NextFunction } from 'express'
+import { get, controller, bodyValidator, post } from './decorators'
 
 
 @controller('/auth')
@@ -20,5 +20,19 @@ class LoginController {
             <button>Submit</button>
             </form>
         `);
+    }
+
+
+    @post('/login')
+    @bodyValidator('email', 'password')
+    postLogin(req: Request, res: Response) {
+        const { email, password } = req.body;
+
+        if(email === 'hi@hi.com' && password === 'password') {
+            req.session = { loggedIn: true };
+            res.redirect('/');
+        } else {
+            res.send('Invalid email or password');
+        }
     }
 }
